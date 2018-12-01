faf5130fe7a3f419942c421819c160e5c94237e5
Ujjwal <ujjwalmishra7@gmail.com>
2018-12-01 02:21:33 -0600

    ssl config working, need certificated for other services

--- gateway/src/main/java/com/piggymetrics/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/piggymetrics/gateway/GatewayApplication.java
@@ -1,9 +1,13 @@
 package com.piggymetrics.gateway;
 
+import java.io.File;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.core.io.Resource;
 
 @SpringBootApplication
 @EnableDiscoveryClient
